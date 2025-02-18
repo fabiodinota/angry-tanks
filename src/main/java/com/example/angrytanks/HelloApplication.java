8418c652b266d655fb9c9856bea00c10c90cbb80
fabiodinota <fabiodinota@gmail.com>
2025-02-18 13:42:13 +0100

    first commit

--- src/main/java/com/example/angrytanks/HelloApplication.java
+++ src/main/java/com/example/angrytanks/HelloApplication.java
@@ -11,10 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Angry Tanks");
         stage.setScene(scene);
-        stage.setMaximized(true);
         stage.show();
     }
 
